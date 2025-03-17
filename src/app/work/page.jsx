3d0f59ecf3a3a72ac64c916d8e4bfc26825b8ae1
Nicolo <71701653+Nicolooo@users.noreply.github.com>
2025-03-17 11:54:58 +0100

    fix

--- src/app/work/page.jsx
+++ src/app/work/page.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import WorkHero from '@/components/work/hero/workHero'
-import Grid from '@/components/work/grid/grid'
 export default function Work() {
   return (
     <>
       <WorkHero />
-      <Grid />
     </>
   )
 }
